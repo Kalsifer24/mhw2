846f1ee340d0eaefbc2f18581c2be6bdb6d1b021
Kalsifer24 <102297248+Kalsifer24@users.noreply.github.com>
2022-04-14 17:19:27 +0200

    Add files via upload

--- constants.js
+++ constants.js
@@ -1,47 +1,47 @@
 ﻿const RESULTS_MAP = {
 'blep': {
-title: 'Velocista',
-contents: "Il vostro sguardo nasconde lo spirito da segugio che è in voi, e sa leggere gli altri e i loro desideri. Ridete quando siete confusi e sembrate straniti quando le cose sono divertenti. Vi piacciono le frasi brevi. Avete sempre voluto avere un giardino; purtroppo, le piante non sono facili da capire come gli umani, e l'ultima volta che vi siete presi  cura di un fiore non è andata così bene. Ad ogni modo, vi prendete buona cura dei vostri amici, che sono fortunati ad avervi.",
+title: 'Bad Guy',
+contents: "Tifi quasi sempre per l'antagonista, non ami le storie a lieto fine e speri che tutto finisca sempre con la distruzione dell'universo.",
 image: "",
 },
 'burger': {
-title: 'Viaggiatore',
-contents: "E' più probabile che siate voi a influenzare le persone intorno piuttosto che il contrario. La qualità del vostro lavoro conta molto per voi. Siete dei pensatori visuali, e sapete trovare analogie tra argomenti diversi. Vi piace condividere scherzi con gli amici. Siete puntigliosi sul cibo, ma vi piace provare un nuovo piatto almeno una volta. Credete che la contraddizione sia alla base della vostra anima. A volte non ve ne rendete conto, ma molte persone vi ammirano.",
+title: 'Sentimentale',
+contents: "Simpatizzi per i personaggi apparentemente più deboli e vulnerabili.",
 image: "",
 },
 'cart': {
-title: 'Nostalgico',
-contents: "Siete calorosi a bravi a migliorare la giornata degli altri. Segretamente, vi piace l'idea di essere dei ribelli, ma la maggior parte di questa ribellione finisce nella vostra testa. Vi piace fare diagrammi di flusso. Volete sempre finire tutto quello che avete in programma per la giornata, e quando siete concentrati è difficile interrompervi. E' importante per voi aver un sfogo creativo e un'indipendenza personale. Siete fortemente motivati, il che è spesso fonte di ispirazione per gli altri.",
+title: 'Boomer',
+contents: "Guardi solo anime usciti prima del 1999 e ti lamenti costantemente di come i nuovi anime in computer grafica e la risoluzione in 16:9 sia sopravvalutata e priva di contenuto.",
 image: "",
 },
 'dopey': {
-title: 'Centauro',
-contents: "Vi piace correre, saltare, scattare, camminare, fare jogging e andare sul monociclo. Il vostro nome utente online è \"L0LxXXxNawt_4_daag_96\". Occasionalmente aggiungete delle faccine vicino alla vostra firma. Indossate segretamente una catenina d'oro con un ciondolo a forma di guanti da boxe e un'etichetta con scritto \"Rocky\". Quando siete soli, però, riflettete sulla crescente commercializzazione della capacità creativa umana e vi piace ripetere Marx mentre camminate nei boschi.",
+title: 'Ghibli fan',
+contents: "Nella tua vita hai visto solo film dello studio ghibli ma ti rapporti con gli altri come se fossi il più grande intenditore di anime in città.",
 image: "",
 },
 'happy': {
-title: '',
-contents: "Siete ottimisti e fiduciosi della vostra abilità di migliorare le cose, che siano esse un gruppo di persone, un processo lavorativo, o voi stessi. E' difficile decidere se siete introversi o estroversi. Avete tanti libri che intendete leggere, e la prossima settimana ne inizierete uno. Quando vi svegliate la mattina, avete difficoltà a ricordare cosa avete letto. Vi piace il suono della pioggia ma non starci in mezzo. Avete paura di non riuscire a fare abbastanza, mente tutti pensano che facciate più di loro.",
+title: 'SIMP',
+contents: "Guardi solo anime romantici con storie d'amore a lieto fine, i tuoi standard di bellezza sono stati totalmente sovrascritti da quelli dei personaggi 2D.",
 image: "",
 },
 'nerd': {
-title: 'Mentalmente aperti e dal cuore grande',
-contents: "Siete fan di Harry Potter, e siete stati scelti per riparare ai torti del mondo e riportare l'armonia nella Terra di Mezzo. Vi piace leggere biografie storiche e fare test di personalità che confermino che la vostra personalità sia più rara di un fiocco di neve sul dorso di un giaguaro. Avete poteri magici, ma vi consiglio di non metterli sul curriculum, perchè la gente potrebbe non capirvi.",
+title: 'Nostalgico',
+contents: "Guardi un pò tutto ma niente ti entusiasma, ogni opera che finisci ti sembra mediocre e pensi che niente sarà mai meglio di Dragon Ball.",
 image: "",
 },
 'shy': {
-title: 'A volte timidi, ma segretamente avventurosi',
-contents: "Siete delle persone molto riflessive, sia quando pensate agli altri che quando pensate a delle idee. Vi piace la natura, in particolare il modo in cui la luce filtra tra le foglie. In un gruppo, non siete quasi mai la prima persona a parlare, ma quando lo fate gli altri vogliono che continuiate. Date più valore al futuro che al passato. Non vi piacciono i giochi di parole e vi piacciono le note scritte a mano; i giochi di parole scritti a mano vanno bene. Non avete paura di affrontare nuovo sfide, il che è molto bello.",
+title: 'Plot-twister',
+contents: "Odi le trame lineari e banali, vuoi sempre rimanere sorpreso dagli eventi narrati ma ti lamenti se il finale non è quello che ti sei creato nella tua testa dopo i primi 5 minuti del primoi episodio.",
 image: ""
 },
 'sleeping': {
-title: 'Liberi e indipendenti',
-contents: "Trovate energia nel fare le cose che ritenete importanti, e diventate irrequieti se vi sembra di essere sempre la stessa persona. Siete bravi ad articolare le idee. Gli orologi sono una delle cose che vi piacciono di meno. Vi separate sempre dal vostro gruppo per esplorare un posto nuovo, e fortunatamente avete un ottimo senso dell'orientamento. Avete un talento nascosto che pochi conoscono, ma tranquilli, manterrò il segreto.",
+title: 'Psicologo',
+contents: "La maggior parte dei dialoghi che ascolti negli anime sono i pensieri interminabili dei personaggi che spiegano come siano riusciti a predirre ogni mossa del proprio avversario avendolo visto di sfuggita per sbaglio per un secondo mentre passeggiavano per strada .",
 image: ""
 },
 'sleepy': {
-title: 'Più forti di quanto pensiate',
-contents: "La chiamate \"meditazione\", ma è più \"chiudere gli occhi tra una sveglia e l'altra\". Lo chiamate \"sollevamento pesi\", ma è più \"una buona scusa per bere Gatorade\". Le vostre battute hanno spesso un buon tempismo e piacciono a tutti. Trascendete le difficoltà della vita, schivandole agilmente. Mangiate i biscotti della fortuna, ma non leggete mai i bigliettini perchè siete già sicuri di voi. Fare pisolini è uno dei vostri talenti, ma quando vi concentrate, vi dedicate al 100% a fare qualcosa.",
+title: 'Otaku',
+contents: "Guardi solo anime di massa e a stento ricordi il nome del protagonista, sparli di ogni opera che non abbia almeno 8/10 su Myanimelist e sei odiato da tutti.",
 image: ""
 }
 };
